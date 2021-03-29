faff0de56d3bc46559f83279d07fbafa3fea401b
mghub55@gmail.com <mghub55@gmail.com>
2021-03-29 11:28:01 +0100

    Created a user-portal file

---
+++ JS/main.js
@@ -0,0 +1,28 @@
+// function userPortal(){
+//     alert(` Welcome User `);
+//     var userName = prompt("Input your Username")
+//     userName;
+// }
+// userPortal()
+// function userAccess(){
+//     alert(`Copy your user ID`);
+//     var userID = Math.random();
+//     var newID = userID;
+//     alert(`${newID}`);
+//     var password = prompt("Input your user Password");      
+
+//     if (password.length >= 8 ){
+//         var uID = prompt("Enter userID")
+//         if(uID == newID)
+//             alert('Succesful')
+//         else{
+//             alert('Incorrect userID')
+//             userAccess()
+//         }
+//     }
+//     else{
+//         alert('Password Must be up to 8 characters')
+//         userAccess()
+//     }
+// }
+// userAccess()
